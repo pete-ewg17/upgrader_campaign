4ca5216397d07fd41a0d6e178cacfcd311bf08b9
Pete Akhurst <peteakhurst@gmail.com>
2017-05-23 20:33:10 +1000

    added extra content

--- js/freelancer.js
+++ js/freelancer.js
@@ -42,3 +42,17 @@
     });
 
 })(jQuery); // End of use strict
+
+
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pauseOnHover: true,
+    interval: 3000,
+  });
+    });
+
+
+
+
