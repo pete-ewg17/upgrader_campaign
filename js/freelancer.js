a47f91709d5a426366ec3cb0509b3ce2ebee17a6
pete-ewg17 <pete@eventworkforcegroup.com>
2017-06-08 13:09:11 +1000

    updated js files

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,12 +1,12 @@
 // When the DOM is ready, run this function
-$(document).ready(function() {
+
   //Set the carousel options
   $('#quote-carousel').carousel({
     pauseOnHover: true,
     interval: 3000,
   });
 
-});
+
 
 (function($) {
     "use strict"; // Start of use strict
@@ -44,68 +44,25 @@ $(document).ready(function() {
 
 
 // Add counter
-$(document).ready(function() {
+
     
     $('.counter-num').counterUp({
-        delay: 0,
-        time: 1000
+        delay: 2,
+        time: 100
     });
-});
-
-// Waypoints 
 
 
-$(document).ready(function() {
-  
-  $(".first").waypoint(function(){
-    $(".first").addClass('animated bounceInUp')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".second").waypoint(function(){
-    $(".second").addClass('animated fadeInLeft')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".third").waypoint(function(){
-    $(".third").addClass('animated fadeInUp')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".fourth").waypoint(function(){
-    $(".fourth").addClass('animated fadeInRight')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".fifth").waypoint(function(){
-    $(".fifth").addClass('animated fadeIn')
-  }, {
-    offset: "90%"
-  });
-  
-  $(".sixth").waypoint(function(){
-    $(".sixth").addClass('animated fadeInRight')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".seventh").waypoint(function(){
-    $(".seventh").addClass('animated fadeInLeft')
-  }, {
-    offset: "100%"
-  });
-  
-  $(".eigth").waypoint(function(){
-    $(".eigth").addClass('animated slideInLeft')
-  }, {
-    offset: "100%"
-  });
 
-});
+// ANIMATE ON SCROLL
+
+     AOS.init({
+  duration: 1200,
+	disable: 'mobile'
+		 })
+  
+
+
+
 
 
 
